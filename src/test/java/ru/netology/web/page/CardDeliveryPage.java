47a69ce48fc5c3ef8e3b0a45e50eab1c3fd5ade0
Albina MV <alyamarvlad@gmail.com>
2022-09-17 23:48:03 +1000

    page object added

---
+++ src/test/java/ru/netology/web/page/CardDeliveryPage.java
@@ -0,0 +1,70 @@
+package ru.netology.web.page;
+
+import com.codeborne.selenide.SelenideElement;
+import lombok.Getter;
+import org.openqa.selenium.Keys;
+import ru.netology.web.data.DataGenerator;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class CardDeliveryPage {
+    private SelenideElement cityField = $("[data-test-id = city] input");
+    private SelenideElement dateField = $("[data-test-id = date] input");
+    private SelenideElement nameField = $("[data-test-id = name] input");
+    private SelenideElement phoneField = $("[data-test-id = phone] input");
+    private SelenideElement agreementBox = $("[data-test-id = agreement]");
+    private SelenideElement sendButton = $$("form button").last();
+    private SelenideElement replanButton = $("[data-test-id = replan-notification] .button");
+    private SelenideElement successNotification = $("[data-test-id = success-notification]");
+    //private SelenideElement error = $("[data-test-id = error-notification]");
+
+    public void sendTheForm(DataGenerator.UserInfo user, String date) {
+        cityField.setValue(user.getCity());
+
+        dateField.doubleClick().sendKeys(" ");
+        dateField.setValue(date);
+
+        nameField.setValue(user.getName());
+        phoneField.setValue(user.getPhone());
+        agreementBox.click();
+        sendButton.click();
+    }
+
+    public void replanDeliveryDate(DataGenerator.UserInfo user, String date) {
+        sendTheForm(user, date);
+        replanButton.click();
+    }
+
+    public void sendFormWithWrongPhone(DataGenerator.UserInfo user, String date) {
+        sendTheForm(user, date);
+        replanButton.click();
+    }
+
+    public void checkDateNotification(String expected) {
+        successNotification.shouldBe(visible, Duration.ofSeconds(15)).shouldHave(text(expected));
+    }
+
+    public void checkError(String expected) {
+        successNotification.shouldBe(visible, Duration.ofSeconds(15)).shouldHave(text(expected));
+    }
+
+    public void clearTheForm() {
+        cityField.sendKeys(Keys.CONTROL + "A");
+        cityField.sendKeys(Keys.BACK_SPACE);
+
+        dateField.doubleClick().sendKeys(" ");
+
+        nameField.sendKeys(Keys.CONTROL + "A");
+        nameField.sendKeys(Keys.BACK_SPACE);
+
+        phoneField.sendKeys(Keys.CONTROL + "A");
+        phoneField.sendKeys(Keys.BACK_SPACE);
+
+        agreementBox.click();
+    }
+}
--- src/test/java/ru/netology/web/test/СhangeDateDeliveryTest.java
+++ src/test/java/ru/netology/web/test/СhangeDateDeliveryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
 import ru.netology.web.data.DataGenerator;
 import ru.netology.web.data.DataGenerator.UserInfo;
+import ru.netology.web.page.CardDeliveryPage;
 
 import java.text.SimpleDateFormat;
 import java.time.Duration;
@@ -17,6 +18,8 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class СhangeDateDeliveryTest {
+    CardDeliveryPage deliveryPage;
+
     @BeforeAll
     static void setUpAll() {
         SelenideLogger.addListener("allure", new AllureSelenide());
@@ -30,6 +33,18 @@ public class СhangeDateDeliveryTest {
     @BeforeEach
     public void setUp() {
         open("http://localhost:9999/");
+        deliveryPage = new CardDeliveryPage();
+    }
+
+    @Test
+    public void shouldPlanDelivery() {
+        UserInfo user = DataGenerator.Registration.generateUser("ru");
+        String date = DataGenerator.generateDate(5, "dd.MM.yyyy");
+
+        deliveryPage.sendTheForm(user, date);
+
+        String expected = "Встреча успешно запланирована на " + date;
+        deliveryPage.checkDateNotification(expected);
     }
 
     @Test
@@ -38,41 +53,12 @@ public class СhangeDateDeliveryTest {
         String dateFirst = DataGenerator.generateDate(6, "dd.MM.yyyy");
         String dateSecond = DataGenerator.generateDate(4, "dd.MM.yyyy");
 
-        sendTheForm(user, dateFirst);
-        clearTheForm();
-        sendTheForm(user, dateSecond);
-        $("[data-test-id = replan-notification] .button").click();
+        deliveryPage.sendTheForm(user, dateFirst);
+        deliveryPage.clearTheForm();
+        deliveryPage.replanDeliveryDate(user, dateSecond);
 
         String expected = "Встреча успешно запланирована на " + dateSecond;
-        $("[data-test-id = success-notification]").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(text(expected));
-
-    }
-
-    public void sendTheForm(UserInfo user, String date) {
-        $("[data-test-id = city] input").setValue(user.getCity());
-
-        $("[data-test-id = date] input").doubleClick().sendKeys(" ");
-        $("[data-test-id = date] input").setValue(date);
-
-        $("[data-test-id = name] input").setValue(user.getName());
-        $("[data-test-id = phone] input").setValue(user.getPhone());
-        $("[data-test-id = agreement]").click();
-        $$("form button").last().click();
-    }
-
-    public void clearTheForm() {
-        $("[data-test-id = city] input").sendKeys(Keys.CONTROL + "A");
-        $("[data-test-id = city] input").sendKeys(Keys.BACK_SPACE);
-
-        $("[data-test-id = date] input").doubleClick().sendKeys(" ");
-
-        $("[data-test-id = name] input").sendKeys(Keys.CONTROL + "A");
-        $("[data-test-id = name] input").sendKeys(Keys.BACK_SPACE);
-
-        $("[data-test-id = phone] input").sendKeys(Keys.CONTROL + "A");
-        $("[data-test-id = phone] input").sendKeys(Keys.BACK_SPACE);
-
-        $("[data-test-id = agreement]").click();
+        deliveryPage.checkDateNotification(expected);
     }
 
 }
